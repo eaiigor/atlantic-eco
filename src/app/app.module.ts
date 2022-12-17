40a5a716aaefb6cf39ceb3559446fa6515dc8a58
Igor Lima <contatoigorrochalima@hotmail.com>
2022-12-17 02:55:18 -0300

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { MainHeaderComponent } from './pages/main-header/components/main-header-component/main-header.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
